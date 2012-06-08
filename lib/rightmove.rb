ecd19ba7e9648a5add0df11871b5a2c3bf787844
Robert May <robotmay@gmail.com>
2012-06-08 13:02:33 +0100

    Fixes typo

--- lib/rightmove.rb
+++ lib/rightmove.rb
@@ -38,7 +38,7 @@ module Rightmove
 
     def time_from_string(string)
       nums = string.split("").map(&:to_i)
-      take = lamba { |nums, i| nums.shift(i).join.to_i }
+      take = lambda { |nums, i| nums.shift(i).join.to_i }
       Time.new(take.call(nums, 4), take.call(nums, 2), take.call(nums, 2),  take.call(nums, 2), take.call(nums, 2), take.call(nums, 2))
     end
 
